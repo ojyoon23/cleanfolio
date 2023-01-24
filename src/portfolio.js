c9cb68fb6ab8d10abff84f0c49b9269210f76538
ojyoon23 <ojyoon23@gmail.com>
2023-01-24 13:44:49 -0800

    edited typo

--- src/portfolio.js
+++ src/portfolio.js
@@ -9,7 +9,7 @@ const about = {
   name: 'Olivia Yoon',
   role: 'Data Analyst',
   description:
-    'My name is Olivia Yoon. I am a Statistical Data Science student at UC Davis, graduating in March 2023. I am most passionate about helping others make smart decisions and my goal is to pursue this passion within the field of data analytics. In my free time, I like working going on a hike, and going to concerts.',
+    'My name is Olivia Yoon. I am a Statistcs student (emphasis on Data Science) at UC Davis, graduating in March 2023. I am most passionate about helping others make smart decisions and my goal is to pursue this passion within the field of data analytics. In my free time, I like listening to different types of music, going to concerts, and hiking.',
   resume:
     'https://drive.google.com/file/d/19FLkIPTDl4tHfIVz4hSbMnzbDCB3oaOY/view?usp=share_link',
   social: {
